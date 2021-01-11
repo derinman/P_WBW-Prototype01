8b5c6b05c63517067a76e23c67d84bfa90408cd0
inman <sf990668852@gmail.com>
2021-01-11 23:25:33 +0800

    add interact effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Suspense, useRef, useMemo } from 'react'
+import React, { useState, Suspense, useRef } from 'react'
 
 import { Canvas, useLoader, useFrame, useThree, extend } from 'react-three-fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+import { useSpring, animated as a } from 'react-spring/three'
 
 import styled from 'styled-components';
 
@@ -17,6 +19,8 @@ const Wrapper = styled.div`
   overflow: hidden;
 `;
 
+
+
 const MartirezRoom = ()=> {
 
   const {nodes} = useLoader(GLTFLoader, MartirezRoomGltf, (loader) => {
@@ -26,12 +30,21 @@ const MartirezRoom = ()=> {
   })
   const group = useRef();
 
-  console.log(nodes)
+  const [ isHover, setIsHover ] = useState(false);
+
+  const {scale1} = useSpring({
+    scale1: isHover ? 0.5 : 1 ,
+    //delay: 500,
+    config: { mass: 1, tension: 280, friction: 120 }
+  })
+
+  console.log(scale1)
 
   return (
     <group
       ref={group}
     >
+      {/* room */}
       <mesh
         geometry={nodes.room.geometry}
         material={nodes.room.material}
@@ -53,29 +66,39 @@ const MartirezRoom = ()=> {
           geometry={nodes.Hand.geometry}
           material={nodes.Hand.material}
       />
-      <mesh
+
+      <a.mesh
           geometry={nodes.Cone.geometry}
           material={nodes.Cone.material}
+          scale={scale1.interpolate(r => [r, r, r])}
       />
-      <mesh
+      <a.mesh
           geometry={nodes.Cube.geometry}
           material={nodes.Cube.material}
+          scale={scale1.interpolate(r => [r, r, r])}
       />
-      <mesh
+      <a.mesh
           geometry={nodes.Icosphere.geometry}
           material={nodes.Icosphere.material}
+          scale={scale1.interpolate(r => [r, r, r])}
       />
-      <mesh
+      <a.mesh
           geometry={nodes.Sphere.geometry}
           material={nodes.Sphere.material}
+          scale={scale1.interpolate(r => [r, r, r])}
       />
-      <mesh
+      <a.mesh
           geometry={nodes.Sphere001.geometry}
           material={nodes.Sphere001.material}
+          scale={scale1.interpolate(r => [r, r, r])}
       />
-      <mesh
+      {/* cloud */}
+      <a.mesh
           geometry={nodes.Sphere002.geometry}
           material={nodes.Sphere002.material}
+          scale={scale1.interpolate(r => [r, r, r])}
+          onPointerOver={() => setIsHover(true)} 
+          onPointerOut={() => setIsHover(false)}
       />
 
     </group>
@@ -109,7 +132,7 @@ function App() {
           <pointLight intensity={2} position={[0, -3, 0]} color={'#f2ca66'} decay={2}/>
           
           <Controls
-            autoRotate
+            //autoRotate
             enablePan={false}
             enableZoom={false}
             enableDamping
